56b6be241f2a8e1db3740f6be9848197a6050712
Grégoire LE CORRE <lecorregregoire@gmail.com>
2024-11-13 17:55:37 +0100

    Petites corrections

--- Code/Grégoire/voiture.py
+++ Code/Grégoire/voiture.py
@@ -1,34 +1,38 @@
-
-
-
-
 #Pour la voiture, on a besoin du centre de la  voiture (juste une paire de points), de son orientation (un vecteur normalisé qui va donner dans quel sens elle se dirige) et sa vitesse
 #(Pour ça on utilise une fonction déjà toute faite comme on a dit précédemen
 #Augmente la vitesse on utilise la fonciton 
 #Pour l'instant on fait seulement un truc binaire pour la vitesse, soit la vitesse est max, soit elle est min
+#Pour que ça marche mieux, on utilise une equation diffénrentielle de la forme dv/dt = c - fv, ça va conger vers une vitesse max égale à c / f
 
-from pygame import event
+import pygame
 from math import cos, sin
 
-VITESSE_MAX = 5
-VITESSE_MIN = -2
+VITESSE_MAX = 3
+VITESSE_MIN = -3
+FROTTEMENT = 1
 VITESSE_ROT = 0.05 #C'est des gradient par seconde ou par appuis. A long terme, il faudra vérifier qu'on ne tourne pas trop 
 
-class Voiture(Dynamic):
+class Voiture():
     def __init__(self):
-        self.centre = (0, 0)
+        self.x_position = 0
+        self.y_position = 0
         self.orientation = 0 #C'est en radient, son vecteur d'orientation sera donc (cos(orientation), sin(orientation))
         self.vitesse = 0
     def update(self, events):
-        self.centre += (cos(self.orientation), sin(self.orientation)) * self.vitesse
+        self.x_position += cos(self.orientation) * self.vitesse
+        self.y_position += sin(self.orientation) * self.vitesse
+        if self.x_position < 0:
+            self.x_position = 0
+        if self.y_position == 0:
+            self.y_position = 0
         for event in events:
-            if event == K_PAGEUP:
+            if event == pygame.K_UP:
                 self.augmente_vitesse()
-            elif event == K_PAGEDOWN:
+            elif event == pygame.K_DOWN:
                 self.diminue_vitesse()
-            elif event == K_RSHIFT:
+            elif event == pygame.K_RIGHT:
                 self.tourne_droite()
-            elif event == K_LSHIFT:
+            elif event == pygame.K_LEFT:
                 self.tourne_gauche()
     def augmente_vitesse(self):
         self.vitesse = VITESSE_MAX
@@ -36,5 +40,5 @@ class Voiture(Dynamic):
         self.vitesse = VITESSE_MIN
     def tourne_droite(self):
         self.orientation -= VITESSE_ROT
-    def tourne_gauche_self(self):
+    def tourne_gauche(self):
         self.orientation += VITESSE_ROT
