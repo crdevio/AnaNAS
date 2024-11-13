1a620f541e3c35c88a2e82171b26e320b17261db
NathBo <nboyer200492@gmail.com>
2024-11-13 14:05:05 +0100

    dynamic

---
+++ Code/Nathan/dynamic.py
@@ -0,0 +1,62 @@
+class Dynamic:
+    def __init__(self,pos) -> None:
+        self.pos = pos
+    def update(self,delta_t):
+        pass
+    def get_pixel_absolute(self):
+        return {}
+    def get_pixel_relative(self):
+        original_dict = self.get_pixel_absolute()
+        new_dict = {(x+self.pos[0],y+self.pos[1]): original_dict[(x,y)] for (x,y) in list(original_dict.keys())}
+        return new_dict
+
+class RedLightGreenLight(Dynamic):
+    def __init__(self, pos, delay,radius) -> None:
+        super().__init__(pos)
+        self.delay = delay
+        self.current_delay = delay
+        self.color = 0
+        self.radius = radius
+        self.reddict = {}
+        for i in range(-radius, radius+1):
+            for j in range(-radius, radius+1):
+                if i**2+j**2<=radius**2:self.reddict[(i,j)] = "red"
+        self.greendict = {pos:"green" for pos in list(self.reddict.keys())}
+
+    def get_pixel_absolute(self):
+        if self.color==0:return self.reddict
+        else:return self.greendict
+
+    def update(self, delta_t):
+        self.current_delay -= delta_t
+        if self.current_delay<=0:
+            self.current_delay = self.delay
+            self.color = 1-self.color
+
+
+class DynamicEnvironnement:
+    def __init__(self) -> None:
+        self.dynamic_objects = []
+    def add(self,static):
+        self.dynamic_objects.append(static)
+    def __getitem__(self, index):
+        return self.dynamic_objects[index]
+    def update_env(self,delta_t):
+        for i in self:
+            i.update(delta_t)
+    def get_pixel_env(self):
+        rep = {}
+        for i in self:
+            rep = rep | i.get_pixel_relative()
+        return rep
+    
+
+env = DynamicEnvironnement()
+env.add(RedLightGreenLight((0,0),1.,2))
+env.add(RedLightGreenLight((20,20),2.,3))
+
+for i in range(10):
+    print(f"New step {i+1}")
+    env.update_env(1.)
+    print(env.get_pixel_env())
+
