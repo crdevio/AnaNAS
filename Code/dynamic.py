5940890fe75fbd6ca271a4e445ccbda075a12879
crdevio <clement.rouvroy80@ens.psl.eu>
2024-11-20 10:33:46 +0100

    je suis l'esclave de Nathan

--- Code/dynamic.py
+++ Code/dynamic.py
@@ -18,7 +18,7 @@ class RedLightGreenLight(Dynamic):
         if self.color==0:return [["circle","green",self.pos,self.radius]]
         else:return [["circle","red",self.pos,self.radius]]
 
-    def update(self, delta_t):
+    def update(self, delta_t, keys = None):
         self.current_delay -= delta_t
         if self.current_delay<=0:
             self.current_delay = self.delay
@@ -32,9 +32,9 @@ class DynamicEnvironnement:
         self.dynamic_objects.append(static)
     def __getitem__(self, index):
         return self.dynamic_objects[index]
-    def update_env(self,delta_t):
+    def update_env(self,delta_t, keys = None):
         for i in self:
-            i.update(delta_t)
+            i.update(delta_t, keys)
     def get_shape_env(self):
         rep = []
         for i in self:
