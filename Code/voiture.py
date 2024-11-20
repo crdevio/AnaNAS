0f715965af3c14d1feac113e36ef3baa6b606472
Grégoire LE CORRE <lecorregregoire@gmail.com>
2024-11-20 10:35:40 +0100

    eognsm

--- Code/voiture.py
+++ Code/voiture.py
@@ -16,31 +16,31 @@ class Voiture():
         self.y_position = 0
         self.orientation = 0 #C'est en radient, son vecteur d'orientation sera donc (cos(orientation), sin(orientation))
         self.vitesse = 0
-    def update(self, events):
-        self.x_position += cos(self.orientation) * self.vitesse
-        self.y_position += sin(self.orientation) * self.vitesse
+    def update(self, dt, events):
+        self.x_position += cos(self.orientation) * self.vitesse * dt
+        self.y_position += sin(self.orientation) * self.vitesse * dt
         accelere = False
         descelere = False
         for event in events:
             if event == pygame.K_UP:
-                self.avance()
+                self.avance(dt)
                 accelere = True
             elif event == pygame.K_DOWN:
-                self.recule()
+                self.recule(dt)
                 descelere = True
             elif event == pygame.K_RIGHT:
-                self.tourne_droite()
+                self.tourne_droite(dt)
             elif event == pygame.K_LEFT:
-                self.tourne_gauche()
+                self.tourne_gauche(dt)
         if (not accelere) and (not descelere):
-            self.ralenti()
-    def avance(self):
-        self.vitesse += 1 / FPS * (VITESSE_MAX - self.vitesse)
-    def recule(self): 
-        self.vitesse += 1 / FPS * (VITESSE_MIN - self.vitesse)
-    def ralenti(self):
-        self.vitesse += 1 / FPS * (0 - self.vitesse)
-    def tourne_droite(self):
-        self.orientation += VITESSE_ROT
-    def tourne_gauche(self):
-        self.orientation -= VITESSE_ROT
+            self.ralenti(dt)
+    def avance(self, dt):
+        self.vitesse += dt * (VITESSE_MAX - self.vitesse)
+    def recule(self, dt): 
+        self.vitesse += dt * (VITESSE_MIN - self.vitesse)
+    def ralenti(self, dt):
+        self.vitesse += dt * (0 - self.vitesse)
+    def tourne_droite(self, dt):
+        self.orientation += VITESSE_ROT * dt
+    def tourne_gauche(self, dt):
+        self.orientation -= VITESSE_ROT * dt
