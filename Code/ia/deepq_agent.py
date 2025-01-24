b85db089d5a23bb0426bbed6aa5edcce606749f4
crdevio <clement.rouvroy80@ens.psl.eu>
2025-01-24 13:39:11 +0100

    print rewards

--- Code/ia/deepq_agent.py
+++ Code/ia/deepq_agent.py
@@ -76,8 +76,8 @@ class DeepQAgent:
                     is_terminal = True
                     print("Final Reward: ", self.memory.rewards[self.memory.mem_index-1])
                     self.eps_decay = EPS_DECAY
-                self.jeu.draw(0)
                 rewards = ia.state_reward(dyn_env.cars[0])
+                self.jeu.draw(rewards)
                 if self.global_t >= WARMUP_PHASE and self.do_opti and self.global_t % MODEL_UPDATE_EVERY == 0:
                     self.optimize_model()
                 for car in dyn_env.cars:
